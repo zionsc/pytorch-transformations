610ce5bb4675570f5f58eb9dd5783c1de32182b0
Seeon Chang <zionchan@usc.edu>
2023-06-29 16:48:47 -0700

    training args

---
+++ finetune.py
@@ -0,0 +1,22 @@
+# 1. prepare dataset
+# 2. load pretrained Tokenizer, call it with dataset -> encoding
+# 3. build PyTorch Dataset with encodings
+# 4. Load pretrained Model
+# 5. a) Load Trainer and train int
+#    b) native PyTorch training loop
+
+from transformers import Trainer, TrainingArguments
+
+training_args = TrainingArguments("test-trainer")
+
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
